270092dc9aa92a542edce28acea19a748b66804d
mbaltman <meggersa@gmail.com>
2022-08-22 23:55:42 -0400

    Can play game
    made play button on start functional. So now it has like a pretty start screen! Will add an opening animation of some sort as well as a credits scene. And need to make a how to page with instructions

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger: MonoBehaviour {
+    public void ScenePlay() {
+        SceneManager.LoadScene("ActiveGamePlay");
+    }
+
+}
